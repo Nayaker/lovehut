fbb89bb25444d1bf17b71b69e72e8636ed243954
Atanu Nayak <93304796+Nayaker@users.noreply.github.com>
2021-11-08 22:24:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,5 @@
 import firebase from "firebase";
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyCneK42bm6-vgyLqfL8fw_WDt7iO-IAvGA",
-    authDomain: "lovehut-nyk.firebaseapp.com",
-    projectId: "lovehut-nyk",
-    storageBucket: "lovehut-nyk.appspot.com",
-    messagingSenderId: "304347013611",
-    appId: "1:304347013611:web:7910d3cebc72093ed979ae"
+    //private
 });
